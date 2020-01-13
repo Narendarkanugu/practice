fc4a753e8c4f02dc6666221a3acffb2b8b90ef88
kalgudi <narendarkanugu19@gmail.com>
2020-01-13 13:36:09 +0530

    mosh project

---
+++ Mosh/counter-app/src/Components/counters.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';  
+import Counter  from './counter';     
+
+
+class Counters extends Component {
+    state = { 
+        Counters: [
+            { id:1, value:4 },
+            { id:2, value:0 },
+            { id:3, value:0 },
+            { id:4, value:0 }
+        ]
+     }
+
+
+     handleDelete =  counterId  => {
+        const Counters =  this.state.Counters.filter(c => c.id !== counterId);
+        this.setState({Counters});
+    }
+ 
+    render() { 
+        return ( 
+            <div>
+                {this.state.Counters.map(counter => 
+                <Counter 
+                    key={counter.id} 
+                    onDelete={this.handleDelete} 
+                    value={counter.value} 
+                    id={counter.id} 
+                />  
+                )}
+            </div>
+         );
+    }
+}
+ 
+export default Counters;
